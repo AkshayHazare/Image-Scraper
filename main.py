ffa1e53b35306a3ea36857c101e256dc032964b5
AkshayHazare <ah4093@nyu.edu>
2018-03-14 14:07:14 -0400

    Version 1.0

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import urllib.request
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+count = 0
+
+
+@app.route("/")
+def main():
+    if count == 1:
+        return render_template("index.html", result=str((str(count) + " Image Downloaded !")))
+    else:
+        return render_template("index.html", result=str((str(count) + " Images Downloaded !")))
+
+
+@app.route("/get_images", methods=['POST'])
+def get_images():
+    _url = request.form['inputURL']
+    try:
+        global count
+        count = 0
+        code = requests.get(_url)
+        text = code.text
+        soup = BeautifulSoup(text)
+        for img in soup.findAll('img'):
+            count += 1
+            if (img.get('src'))== None:
+                continue
+            if (img.get('src'))[0:4] == 'http':
+                src = img.get('src')
+            else:
+                src = urljoin(_url, img.get('src'))
+            download(src, count)
+        return redirect("http://localhost:7777")
+    except requests.exceptions.HTTPError as error:
+        return render_template("index.html", result=str(error))
+
+
+def download(url, num):
+    try:
+        image_name = str(num) + (".jpg" or ".png" or ".gif")
+        image_path = os.path.join("images/", image_name)
+        urllib.request.urlretrieve(url, image_path)
+    except ValueError:
+        print("Invalid URL !")
+    except Exception as e:
+        print("Unknown Exception" + str(e))
+
+
+if __name__ == "__main__":
+    app.run(port=7777)
